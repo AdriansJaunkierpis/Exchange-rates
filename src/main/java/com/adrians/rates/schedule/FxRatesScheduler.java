a5f4d9dddf1a31c931dd0f8a75e2361e04a0c24d
AdriansJaunkierpis <adrians.jaunkierpis@gmail.com>
2025-11-02 21:19:18 +0200

    Schedule obtaining daily rate

---
+++ src/main/java/com/adrians/rates/schedule/FxRatesScheduler.java
@@ -0,0 +1,18 @@
+package com.adrians.rates.schedule;
+
+import com.adrians.rates.service.FxRatesService;
+import org.springframework.scheduling.annotation.Scheduled;
+
+public class FxRatesScheduler {
+    private final FxRatesService fxRatesService;
+
+    public FxRatesScheduler(FxRatesService fxRatesService) {
+        this.fxRatesService = fxRatesService;
+    }
+
+    // Every day at 03:00 AM
+    @Scheduled(cron = "0 0 3 * * ?")
+    public void fetchDailyRates() {
+        fxRatesService.getCurrentFxRates();
+    }
+}
