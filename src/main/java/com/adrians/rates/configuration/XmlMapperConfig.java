1a41d49bf52bd1f715db72fa0c779a7cc987c823
AdriansJaunkierpis <adrians.jaunkierpis@gmail.com>
2026-02-05 20:37:55 +0200

    Fix configuration for error handling

--- src/main/java/com/adrians/rates/configuration/XmlMapperConfig.java
+++ src/main/java/com/adrians/rates/configuration/XmlMapperConfig.java
@@ -1,14 +1,16 @@
 package com.adrians.rates.configuration;
 
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestClient;
 
 @Configuration
-public class FxRatesConfig {
+public class XmlMapperConfig {
     @Bean
-    public RestClient restClient() {
-        return RestClient.builder().build();
+    public XmlMapper xmlMapper() {
+        return XmlMapper.builder()
+                .addModule(new JavaTimeModule())
+                .build();
     }
 }
